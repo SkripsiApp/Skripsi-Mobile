f0028846f8e6c2a5ccb2a8d38f296fe17fd9fe9b
exoream <shiigetsu1@gmail.com>
2024-11-29 14:27:59 +0800

    fix: card voucher

--- lib/ui/voucher/voucher_screen.dart
+++ lib/ui/voucher/voucher_screen.dart
@@ -22,15 +22,15 @@ class _VoucherScreenState extends State<VoucherScreen> {
       body: Column(
         children: [
           Container(
-            padding: const EdgeInsets.all(16),
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
             child: Row(
               children: [
                 Image.asset(
-                  'assets/img/discount.png',
-                  width: 50,
-                  height: 50,
+                  'assets/img/voucher2.png',
+                  width: 70,
+                  height: 70,
                 ),
-                const SizedBox(width: 16),
+                const SizedBox(width: 14),
                 const Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -39,6 +39,7 @@ class _VoucherScreenState extends State<VoucherScreen> {
                       style: TextStyle(
                         fontSize: 24,
                         fontWeight: FontWeight.bold,
+                        color: Color(0xFF3ABEF9),
                       ),
                     ),
                     Text(
@@ -54,7 +55,7 @@ class _VoucherScreenState extends State<VoucherScreen> {
           ),
           Expanded(
             child: ListView(
-              padding: const EdgeInsets.all(16),
+              padding: const EdgeInsets.only(top: 0, left: 16, right: 16),
               children: [
                 _buildVoucherCard(
                   'Promo Diskon 30%',
@@ -73,6 +74,12 @@ class _VoucherScreenState extends State<VoucherScreen> {
                   'Diskon 20% untuk semua aksesoris titanium. Dapatkan sekarang!',
                   'DISKON20',
                 ),
+                const SizedBox(height: 16),
+                _buildVoucherCard(
+                  'Promo Diskon 20%',
+                  'Diskon 20% untuk semua aksesoris titanium. Dapatkan sekarang!',
+                  'DISKON20',
+                ),
               ],
             ),
           ),
@@ -80,7 +87,7 @@ class _VoucherScreenState extends State<VoucherScreen> {
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
-        currentIndex: 3, // Voucher tab is selected
+        currentIndex: 3,
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
@@ -117,56 +124,70 @@ class _VoucherScreenState extends State<VoucherScreen> {
       ),
       child: Padding(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Stack(
           children: [
-            Row(
-              children: [
-                Image.asset(
-                  'assets/img/discount.png',
-                  width: 40,
-                  height: 40,
+             Positioned(
+              top: -30,
+              right: -40,
+                child: Image.asset(
+                  'assets/img/diamond.png',
+                  width: 200,
+                  height: 200,
                 ),
-                const SizedBox(width: 12),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Image.asset(
+                      'assets/img/voucher.png',
+                      width: 40,
+                      height: 40,
+                    ),
+                    const SizedBox(width: 12),
+                    Text(
+                      title,
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.lightBlue,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 12),
                 Text(
-                  title,
+                  description,
                   style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.lightBlue,
+                    color: Colors.grey,
+                    fontSize: 14,
+                  ),
+                ),
+                const SizedBox(height: 12),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.lightBlue,
+                    minimumSize: const Size(double.infinity, 40),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  child: Text(
+                    promoCode,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ],
-            ),
-            const SizedBox(height: 12),
-            Text(
-              description,
-              style: const TextStyle(
-                color: Colors.grey,
-                fontSize: 14,
-              ),
-            ),
-            const SizedBox(height: 12),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.lightBlue,
-                minimumSize: const Size(double.infinity, 40),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-              ),
-              child: Text(
-                promoCode,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
             ),
           ],
         ),
       ),
     );
   }
+
 }
