75efad4e6fecd260c8a05b0fbfd91480db64dba5
exoream <shiigetsu1@gmail.com>
2024-12-24 22:28:55 +0800

    feat: consume api chatbot

---
+++ lib/controller/chatbot_controller.dart
@@ -0,0 +1,67 @@
+import 'package:get/get.dart';
+import 'package:skripsi_app/response/chatbot_response.dart';
+import 'package:skripsi_app/service/service.dart';
+
+class ChatbotController extends GetxController {
+  final ApiService _apiService = ApiService();
+
+  final RxList<ChatMessage> messages = <ChatMessage>[].obs;
+  final RxBool isLoading = false.obs;
+
+  void sendMessage(String question) async {
+    if (question.trim().isEmpty) return;
+
+    // Add user message to the list
+    messages.add(ChatMessage(
+      content: question,
+      isUser: true,
+      timestamp: DateTime.now(),
+    ));
+
+    isLoading.value = true;
+
+    try {
+      ChatbotResponse response = await _apiService.chatbot(question);
+
+      if (response.status) {
+        // Add bot message to the list
+        messages.add(ChatMessage(
+          content: response.data?.answer ?? 'Sorry, I couldn\'t process that.',
+          isUser: false,
+          timestamp: DateTime.now(),
+          images: response.data?.images,
+        ));
+      } else {
+        // Add error message
+        messages.add(ChatMessage(
+          content: response.message,
+          isUser: false,
+          timestamp: DateTime.now(),
+        ));
+      }
+    } catch (e) {
+      // Add error message in case of exception
+      messages.add(ChatMessage(
+        content: 'An error occurred. Please try again.',
+        isUser: false,
+        timestamp: DateTime.now(),
+      ));
+    }
+
+    isLoading.value = false;
+  }
+}
+
+class ChatMessage {
+  final String content;
+  final bool isUser;
+  final DateTime timestamp;
+  final List<String>? images;
+
+  ChatMessage({
+    required this.content,
+    required this.isUser,
+    required this.timestamp,
+    this.images,
+  });
+}
